de1726f46117b1033476572e3e776fdfdb3a3616
Saahil615 <92013787+Saahil615@users.noreply.github.com>
2021-10-09 16:27:13 +0530

    Add files via upload

--- Bus.java
+++ Bus.java
@@ -1,58 +1,67 @@
-public class Bus{
-int x;
-int y;
-int max_cap = 10;
-int current_cap = 0;
-boolean stopped = false;
-Pathfinder path = new Pathfinder();
-
-public Bus(int x1, int y1)
+public class Bus
 {
-  x = x1;
-  y = y1;
-}
-public static void main(String[] args){
-  Bus obj = new Bus(1,1);
-  obj.addGoal(new Position(2,1,1));
-  obj.addGoal(new Position(1,2,1));
-  obj.update();
-  obj.update();
-  obj.update();
-  obj.update();
-}
-public void update(){
-  Position p = path.update(x,y);
-  if(p.no == -1){
-    return;
+  int x;
+  int y;
+  int max_cap = 10;
+  int current_cap = 0;
+  boolean stopped = false;
+  Pathfinder path = new Pathfinder();
+
+  public Bus(int x1, int y1)
+  {
+    x = x1;
+    y = y1;
   }
-  Move(p.x,p.y);
-  System.out.println(x+","+y);
-}
-public boolean addPickup(Position p){
-  if(p.no<=max_cap-current_cap){
-    addGoal(p);
-    return true;
+  public void drop()
+  {
+    current_cap-=path.removed;
+    path.removed = 0;
+    System.out.println(current_cap);
+    System.out.println(path.queue.size());
   }
-  return false;
-}
-public void addGoal(Position p){
-  path.addP(p);
-}
-public void Move(int xoA, int yoA){
-  stopped = false;
-  if(x > xoA){
-    x--;
+  public void update()
+  {
+    Position p = path.update(x,y);
+    if(p.no == -1)
+    {
+      return;
+    }
+    Move(p.x,p.y);
   }
-  else if(x < xoA){
-    x++;
+  public boolean addPickup(Position p)
+  {
+    if(p.no<=max_cap-current_cap)
+    {
+      addGoal(p);
+      return true;
+    }
+    return false;
   }
-  else if(y > yoA){
-    y--;
+  public void addGoal(Position p)
+  {
+    path.addP(p);
   }
-  else if(y < yoA){
-    y++;
-  }else{
-    stopped = true;
+  public void Move(int xoA, int yoA)
+  {
+    stopped = false;
+    if(x > xoA)
+    {
+      x--;
+    }
+    else if(x < xoA)
+    {
+      x++;
+    }
+    else if(y > yoA)
+    {
+      y--;
+    }
+    else if(y < yoA)
+    {
+      y++;
+    }else
+    {
+      stopped = true;
+    }
   }
-}
 }
